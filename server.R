767148922ab2664868472fd39a5b8e47c32a5780
Rong Chen <rongchen1983@gmail.com>
2015-05-12 11:56:09 -0700

    server

--- server.R
+++ server.R
@@ -11,19 +11,18 @@ shinyServer(
     function(input, output) {
         #find model part
         output$newHist <- renderPlot({
-            y<-galton$child - mean(galton$child)
-            x <- galton$parent - mean(galton$parent)
-            freqData <- as.data.frame(table(x, y))
+            freqData <- as.data.frame(table(galton$parent, galton$child))
             names(freqData) <- c("child", "parent", "freq")
             plot(as.numeric(as.vector(freqData$parent)),
                  as.numeric(as.vector(freqData$child)),
                  pch = 21, col = "black", bg = "lightblue",
                  cex = .15 * freqData$freq,
                  xlab = "parent",ylab = "child")
-            abline(0, input$beta, lwd = 3)
-            mse <- mean( (y - input$beta * x)^2 )
-            title(paste("beta = ", input$beta, "mse = ", round(mse, 3)))
+            abline(input$a,input$b,lwd = 3)
+            mse <- mean((galton$child-galton$parent*input$b-input$a)^2)
+            title(paste("intercept =",input$a,"slope=",input$b,"mse = ", round(mse, 3)))
         })
+        
         #prediction part
         output$inputValue <- renderPrint({input$parent_height})
         output$prediction <- renderPrint({bestfit(input$parent_height)})   
