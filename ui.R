487e194a03b70b072ecd13ff729dfd0860fee1d4
Rong Chen <rongchen1983@gmail.com>
2015-05-12 11:56:19 -0700

    ui

--- ui.R
+++ ui.R
@@ -4,16 +4,17 @@ shinyUI(pageWithSidebar(
     headerPanel("Prediction with Galton's Data"),
     #two inputs "beta" and "parent's height"
     sidebarPanel(
-        sliderInput('beta', "beta",1, min = 0.4, max = 1.2, step = 0.02),
+        sliderInput('a', "intercept",23, min = 15, max = 30, step = 0.5),
+        sliderInput('b', "slope",0.6, min = 0.5, max = 1, step = 0.005),
         sliderInput('parent_height', "Parent's Height",68, min = 60, max = 75, step = 0.05)
     ),
     
     mainPanel(
         #find model part
         h3('Find best model using Linear Regression Model'),
-        h4('change beta(slope) to find the best model which make mse smallest'),
+        h4('change intercept and slope to find the best model which make mse smallest'),
         plotOutput('newHist'),
-        h3('Results of prediction with best model when beta=0.64'),
+        h3('Results of prediction with best model when intercept=23.94,slope=0.646'),
         #prediciton part
         h4("The parent's heght you entered"),
         verbatimTextOutput("inputValue"),
