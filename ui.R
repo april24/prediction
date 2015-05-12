0274239436819bcaf138ac9bdc1f3a7a668c5448
Rong Chen <rongchen1983@gmail.com>
2015-05-11 18:23:13 -0700

    ui

---
+++ ui.R
@@ -0,0 +1,23 @@
+library(shiny)
+shinyUI(pageWithSidebar(
+    #Application title
+    headerPanel("Prediction with Galton's Data"),
+    #two inputs "beta" and "parent's height"
+    sidebarPanel(
+        sliderInput('beta', "beta",1, min = 0.4, max = 1.2, step = 0.02),
+        sliderInput('parent_height', "Parent's Height",68, min = 60, max = 75, step = 0.05)
+    ),
+    
+    mainPanel(
+        #find model part
+        h3('Find best model using Linear Regression Model'),
+        h4('change beta(slope) to find the best model which make mse smallest'),
+        plotOutput('newHist'),
+        h3('Results of prediction with best model when beta=0.64'),
+        #prediciton part
+        h4("The parent's heght you entered"),
+        verbatimTextOutput("inputValue"),
+        h4("Prediction of child's height"),
+        verbatimTextOutput("prediction")
+    )
+))
